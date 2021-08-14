f1f67d791f705372079afc00a9453d05f1d132a9
AleksanderLitvinovich <bassboom37@gmail.com>
2021-08-14 12:56:49 +0300

    Изменение настроек (гит) + табуляция

--- sandbox/src/main/java/ru/java/qa/sandbox/test1.java
+++ sandbox/src/main/java/ru/java/qa/sandbox/test1.java
@@ -2,8 +2,8 @@ package ru.java.qa.sandbox;
 
 public class test1 {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, world");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
 
 }
